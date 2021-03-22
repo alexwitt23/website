3f869b16fe96cb1dd53863d44b4750d4bab131a4
alexwitt23 <awitt2399@utexas.edu>
2021-03-21 22:50:50 -0500

    center pics

--- src/Components/Projects/Galerkin.js
+++ src/Components/Projects/Galerkin.js
@@ -7,7 +7,7 @@ class Galerkin extends Component {
     return (
       <div className="row">
         <div className="column-card">
-          <div key={"uav"} className="columns portfolio-item">
+          <div key={"uav"} className="portfolio-item">
             <div className="item-wrap">
               <a href={"https://github.com/alexwitt23/TrussSolver"} title={"galerkin"}>
                 <img alt={"galerkin"} src={imgPath} />
